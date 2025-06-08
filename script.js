9c33f60c9ed7ff286c6a221511e0bff8dc973aaa
David <joshowadee@gmail.com>
2025-06-08 09:45:55 +0200

    Game done
    All functions and variables added.

--- script.js
+++ script.js
@@ -15,17 +15,87 @@ function getComputerChoice() {
     } else {
         decision = "scissor";
     }
+    console.log(decision);
     return decision;
 }
 
 function humanChoice() {
-    let choice = prompt("Please pick between rock, paper and or scissors:")
-    return choice;
+    let choice = prompt("Please pick between rock, paper and or scissors:");
+    return choice.toLowerCase();
 }
+
+//function gets output from player and computer
+//compares the two to find a winner
+//increments thier score 
 
 function playRound(humanChoice, getComputerChoice) {
-
+    if ((humanChoice==="rock") && (getComputerChoice==="paper")) {
+        computerScore++;
+        console.log("You lose!");
+    } else if ((humanChoice=="paper") && (getComputerChoice==="scissor")) {
+        computerScore++;
+        console.log("You lose!");
+    } else if ((humanChoice==="scissor") && (getComputerChoice==="rock")) {
+        computerScore++;
+        console.log("You lose!");
+    } else if ((humanChoice==="scissor") && (getComputerChoice==="paper")) {
+        humanScore++;
+        console.log("You Win!");
+    } else if ((humanChoice==="rock") && (getComputerChoice==="scissor")) {
+        humanScore++;
+        console.log("You Win!");
+    } else if ((humanChoice==="paper") && (getComputerChoice==="rock")) {
+        humanScore++;
+        console.log("You Win!");
+    } else if (humanChoice === getComputerChoice) {
+        console.log("Draw");
+    }
 }
 
-console.log(getComputerChoice());
-console.log(humanChoice());
+
+
+function playGame() {
+    let humanSelection = humanChoice();
+    let ComputerSelection = getComputerChoice();
+
+    playRound(humanSelection, ComputerSelection);
+
+    console.log(humanScore);
+    console.log(computerScore);
+
+    humanSelection = humanChoice();
+    ComputerSelection = getComputerChoice();
+
+    playRound(humanSelection, ComputerSelection);
+
+    console.log(humanScore);
+    console.log(computerScore);
+
+    humanSelection = humanChoice();
+    ComputerSelection = getComputerChoice();
+
+    playRound(humanSelection, ComputerSelection);
+
+    console.log(humanScore);
+    console.log(computerScore);
+
+    humanSelection = humanChoice();
+    ComputerSelection = getComputerChoice();
+
+    playRound(humanSelection, ComputerSelection);
+
+    console.log(humanScore);
+    console.log(computerScore);
+
+    humanSelection = humanChoice();
+    ComputerSelection = getComputerChoice();
+
+    playRound(humanSelection, ComputerSelection);
+
+    console.log(humanScore);
+    console.log(computerScore);
+
+    
+}
+
+playGame();
